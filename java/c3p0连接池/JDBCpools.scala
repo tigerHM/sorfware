bf1ec1d1ca695f633550334fedf6dabb3d2249c3
tigerHM <197480460@qq.com>
2019-06-02 09:51:51 -0500

    Add files via upload

---
+++ java/c3p0连接池/JDBCpools.scala
@@ -0,0 +1,35 @@
+package com.kafka
+
+import java.sql.Connection
+import com.mchange.v2.c3p0.ComboPooledDataSource
+
+
+/**
+  * 使用C3P0连接池进行数据库连接
+  * @
+  *
+  */
+object JDBCpools {
+  private val pooledDataSource = new ComboPooledDataSource()
+  private var connection: Connection = null
+
+  /**
+    * 创建数据库连接
+    * @return
+    */
+  private def getConnection: Connection = {
+    connection = pooledDataSource.getConnection
+    connection
+  }
+
+  /**
+    * 释放连接
+    * @param connection
+    */
+  private def releaseConnection(connection: Connection) = {
+    if( connection!= null){
+      connection.close()
+    }
+  }
+
+}
